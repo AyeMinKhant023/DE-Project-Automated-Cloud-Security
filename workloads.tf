cc52bf2afc3e04fc25b6d66732077db4575d28a5
Aye Min Khant <6630613023@psu.ac.th>
2026-04-30 01:48:39 +0700

    Updating IP address

--- workloads.tf
+++ workloads.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["110.164.247.21/32"] ## use my specific IP to protect anyone to connect my server (especially protecting bruth-force attack)
+    cidr_blocks = ["49.237.37.64/32"] ## use my specific IP to protect anyone to connect my server (especially protecting bruth-force attack)
   }
 
   # Allow HTTP (Port 5000) so that it can see my Flask app
